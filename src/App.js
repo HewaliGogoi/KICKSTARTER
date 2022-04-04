0f0d13aca3168b6f21a7a0aedf0a34f9c923cd87
HewaliGogoi <45413335+HewaliGogoi@users.noreply.github.com>
2022-04-04 19:04:59 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {Routes, Route} from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Creative work shows us what’s possible.</h1>
+        <h1>Help fund it here.</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default Home;
