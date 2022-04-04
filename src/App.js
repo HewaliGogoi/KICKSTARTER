976026f116c1e6c9d4b6419d0eebb037a7ae2c54
sumitra05 <sumitra9039sumi@gmail.com>
2022-04-04 19:01:30 +0530

    Layout of login and signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {Routes, Route} from "react-router-dom";
+import Login from './components/Login';
+// import {Routes, Route} from "react-router-dom";
+import SignUp  from './components/SignUp';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
+      {/* <Login/> */}
+      <SignUp/>
     </div>
   );
 }
---
+++ src/components/Style.module.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width:33%;
+    height: 600px;
+    background-color: white;
+    gap: 30px;
+    border: 1px solid black;
+    margin: auto;
+    margin-top: 70px;
+
+`
