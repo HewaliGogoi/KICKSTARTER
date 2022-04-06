7ec1c32064f297b8af8adb4be169363afbca7a0a
Amarjeet Maurya <30472038+amar969@users.noreply.github.com>
2022-04-06 06:07:46 +0530

    start Project

--- src/components/startProject.jsx
+++ src/components/startProject.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
+const Heading = styled.div`
+
+    button{
+        color: white;
+        background-color: #028858; 
+        padding: 10px 20px;
+        border: none;
+    }
+
+    h1{
+        font-size: 3.0rem;
+        line-height: 5.4rem;
+        font-weight: bold;
+    }
+    
+`
+
 function StartProject(){
     return(
         <>
+            <Heading>
             <h1>Bring your creative project to life.</h1>
             <button>Start a project</button>
-
+            </Heading>
         </>
     )
 }
