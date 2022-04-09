46af5b1c054f391902bd4b1c140731abd3c558b5
HewaliGogoi <45413335+HewaliGogoi@users.noreply.github.com>
2022-04-09 10:03:35 +0530

    completed payment page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -197,7 +197,7 @@ const Interview = styled.div`
     // border: 1px solid red;
     width:380px;
     padding:5% 5%;
-    // height:1000px;
+    height:400px;
   }
 `;
 
@@ -206,7 +206,27 @@ const Box = styled.div`
     // border: 1px solid green;
     width:340px;
     // padding:5% 5%;
-    display:inline-block;
+    // display:none;
+    // padding:1%;
+
+    div{
+      // border:2px solid brown;
+      // padding:1%;
+      position:relative;
+      
+      img{
+        // border:2px solid blue;
+        width:10px;
+        // position:absolute;
+        height:100px !important;
+      }
+      h6{
+        font-size:15px !important;
+        // position:absolute;
+        justify-content:center;
+      }
+    }
+
   }
 `;
 
@@ -248,7 +268,7 @@ const Home = ({setClose, setCloseF}) => {
   return (
     <HomeWrapper>
       <div style={{position:"relative"}}>
-        <BackImg src={right} style={{position:"absolute", left:0, width:"150px", top:"30px"}} alt="" />
+        <BackImg src={right} style={{position:"absolute", left:0, width:"150px", top:"30px", zIndex:"-10"}} alt="" />
         <QuoteWrapper>
           <h1>Creative work shows us what’s possible.</h1>
           <h1>Help fund it here.</h1>
@@ -275,7 +295,7 @@ const Home = ({setClose, setCloseF}) => {
       <BodyWrapper>
         <Overview>
           <FeaturedWrapper>
-            <p>FEATURED PROJECT</p>
+            <p style={{color:"#656977"}}>FEATURED PROJECT</p>
             <img src="https://ksr-ugc.imgix.net/assets/036/376/565/baf56221a9a9e48409de65c6f4eec67c_original.png?ixlib=rb-4.0.2&crop=faces&w=1024&h=576&fit=crop&v=1644987976&auto=format&frame=1&q=92&s=c1153462ed6ce402483846711c2c79b9" alt="" />
             <Raiser style={{width:"30%"}}></Raiser>
             <h3>Where We Go Together or The Flashlight Play</h3>
@@ -283,7 +303,7 @@ const Home = ({setClose, setCloseF}) => {
             <p>By Kevin Michael Wesson</p>
           </FeaturedWrapper>
           <RecommendWrapper>
-            <p>RECOMMENDED FOR YOU</p>
+            <p style={{color:"#656977"}}>RECOMMENDED FOR YOU</p>
             {
               imgData.map((e) => <Pagination e={e} handleImage={handleImage}/>)
             }
--- src/components/Pledge.jsx
+++ src/components/Pledge.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import {useSelector} from 'react-redux';
 
@@ -28,7 +28,17 @@ const Box = styled.label`
 `;
 
 const Pledge = () => {
-    const id = useParams();
+    // const id = useParams();
+
+    const [details, setDetails] = useState({
+        cardnumber : "",
+        cardholdername : "",
+        date : "",
+        cvc :"",
+        pin : ""
+    })
+
+    const navigate = useNavigate();
 
     // useEffect(() => {
     //     getData();
@@ -40,47 +50,78 @@ const Pledge = () => {
     //     .then((res)=>console.log(res))
     // } 
 
-    const single = useSelector(state=>state.single);
+    // const single = useSelector((state)=>state.single);
+    // console.log(single.single)
+
+    const handleChange =(e)=>{
+        e.preventDefault();
+        const {name, value} = e.target;
+        setDetails({
+            ...details,
+            [name] : value
+        })
+        console.log(details)
+    }
+
+    const handlePledge = (e) => {
+        e.preventDefault();
+
+        localStorage.setItem("payment", JSON.stringify(details))
+        alert("Thank you for Pledging!")
+        navigate("/");
+    }
+
+    const {cardholdername, cardnumber, date, cvc, pin} = details;
+    const [amount, setAmount] = useState(0);
+
+    const handleAmount =(e)=>{
+        setAmount(e.target.value);
+    }
+
   return (
     <PledgeWrapper>
         <Summary>
             <h3>Pledge summary</h3>
             <p>We won’t charge you at this time. If the project reaches its funding goal, your payment method will be charged when the campaign ends. You’ll receive a confirmation email at hewaligogoi22@gmail.com when your pledge is successfully processed.</p>
-            <div style={{display:"flex"}}>
+            {/* <div style={{display:"flex"}}>
                 <img src={single.image} style ={{width:"30%"}}alt="" />
                 <div style={{paddingLeft:"5%"}}>
                     <h6>{single.title}</h6>
                     <p>By {single.author}</p>
                 </div>
-            </div>
+            </div> */}
             <h5 style={{paddingTop:"5%"}}>Your pledge</h5>
-            <p style={{margin:"5% 0%"}}>US$<input type="text" name="amount" placeholder="Enter your amount"/></p>
-            <h6 style={{borderTop:"1px solid #cecece"}}>Total amount</h6>
+            <p style={{margin:"5% 0%"}}>US$<input onChange={handleAmount} type="text" name="amount" value={amount} placeholder="Enter your amount"/></p>
+            <h6 style={{borderTop:"1px solid #cecece"}}>Total amount {amount}</h6>
         </Summary>
         <Payment>
             <Box htmlFor="">
-                Card number
-                <input type="text" placeholder="Card number" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
+                <div style={{display:"flex", justifyContent:"space-between"}}>
+                    <h6>Card number</h6>
+                    <img style= {{width:"10%"}} src="https://www.nicepng.com/png/detail/54-542683_credit-card-pay-now-visa-and-mastercard-accepted.png" alt="" />
+                </div>
+                
+                <input type="text" onChange={handleChange} name="cardnumber" value={cardnumber} placeholder="Card number" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
             </Box>
             <Box htmlFor="">
                 Cardholder name
-                <input type="text" placeholder="Cardholder name" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
+                <input type="text" onChange={handleChange} name="cardholdername" value={cardholdername} placeholder="Cardholder name" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
             </Box>
             <div style={{display:"flex", justifyContent:"space-between"}}>
                 <Box htmlFor="" style={{width:"50%"}}>
                     Expiration
-                    <input type="text" placeholder="MM/YY" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
+                    <input type="date" onChange={handleChange} name="date" value={date} placeholder="MM/YY" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
                 </Box>
                 <Box htmlFor="" style={{width:"50%"}}>
                     Security code
-                    <input type="text" placeholder="CVC" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
+                    <input type="text" onChange={handleChange} name="cvc" value={cvc} placeholder="CVC" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
                 </Box>
             </div>
             <Box htmlFor="">
                 Zip/Postal code
-                <input type="text" placeholder="Zip/Postal code" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
+                <input type="text" onChange={handleChange} name="pin" value={pin} placeholder="Zip/Postal code" style={{fontStyle:"italic", border:"1px solid #cecece", padding:"2%", margin:"1%"}}/>
             </Box>
-            <button style={{width:"100%", padding:"2%", backgroundColor:"#037362", color:"white"}}>Pledge</button>
+            <button style={{width:"100%", padding:"2%", backgroundColor:"#037362", color:"white"}} onClick={handlePledge}>Pledge</button>
         </Payment>
     </PledgeWrapper>
   )
