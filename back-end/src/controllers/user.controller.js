b642737a6dc2bc0d0125f2b2b7e9946f3ceb41eb
sumitra05 <sumitra9039sumi@gmail.com>
2022-05-04 17:41:31 +0530

    authentication added

---
+++ back-end/src/controllers/user.controller.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// const { append } = require("express/lib/response");
+const User = require("../models/user.model");
+const router = express.Router();
+const protect = require("../middlewares/protect");
+
+router.get("/user",protect, async(req, res) =>{
+    // console.log("req user",await req.user)
+
+    const users = await User.find({}).select('-password').lean().exec();
+
+    return res.status(200).json({data: users});
+});
+
+module.exports = router;
